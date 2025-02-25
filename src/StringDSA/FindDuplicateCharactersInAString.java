ede1735374464178dffb46f6365726e24d07b365
majid.f <majid.f@wealthmax.co.uk>
2025-02-25 16:58:59 +0530

    FindDuplicateCharactersInAString

--- src/StringDSA/FindDuplicateCharactersInAString.java
+++ src/StringDSA/FindDuplicateCharactersInAString.java
@@ -1,14 +1,22 @@
 package StringDSA;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public interface FindDuplicateCharactersInAString {
-    public static void main(String[] args) {
-        String str = "programming";
-        String[] arr = str.split("");
-        Map<String, Long> map  = Arrays.stream(arr).collect(Collectors.groupingBy(e->e ,Collectors.counting()));
-        map.entrySet().stream().filter(e->e.getValue() > 1).forEach(e-> System.out.print(e.getKey() + " "));
+        public static void main(String[] args) {
+            String str = "programming";
 
+            Set<String> seen = new HashSet<>();
+            Set<String> duplicates = new LinkedHashSet<>();
+
+            Arrays.stream(str.split("")).forEach(e -> {
+                if (!seen.add(e)) {
+                    duplicates.add(e);
+                }
+            });
+            System.out.println(duplicates);
+
+        }
     }
-}
+
+
