22a890677a831d95e72d2357b35bbd723f0c5240
majid.f <majid.f@wealthmax.co.uk>
2025-02-25 16:25:17 +0530

    CheckIfAStringIsAPalindrome

--- src/StringDSA/ReverseAString.java
+++ src/StringDSA/ReverseAString.java
@@ -3,7 +3,7 @@ package StringDSA;
 public class ReverseAString {
     public static void main(String[] args) {
         String str = "majidsaba";
-        String str1 = new StringBuilder(str).reverse().toString();
+        String str1 = new StringBuffer(str).reverse().toString();
         System.out.println(str1);
     }
 }
