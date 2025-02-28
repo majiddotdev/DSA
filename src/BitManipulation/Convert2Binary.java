e184dadf4cbc753e8ac403f564af003d6d2ae19c
majid.f <majid.f@wealthmax.co.uk>
2025-02-28 14:45:16 +0530

    Convert2Binary

---
+++ src/BitManipulation/Convert2Binary.java
@@ -0,0 +1,14 @@
+package BitManipulation;
+
+public class Convert2Binary {
+    public static void main(String[] args) {
+        int number = 13;
+        StringBuilder newNumber = new StringBuilder();
+        while (number > 0){
+           if(number % 2 == 1) newNumber.append(1);
+           else  newNumber.append(0);
+           number = number / 2;
+        }
+        System.out.println(newNumber.reverse().toString());
+    }
+}
