93e74af411d2bd75685dcb030a65cdc94fd201af
majid.f <majid.f@wealthmax.co.uk>
2025-03-01 12:47:48 +0530

    CapitalFirstLetter

--- src/CapitalFirstLetter.java
+++ src/CapitalFirstLetter.java
@@ -1,18 +1,27 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class CapitalFirstLetter {
     public static void main(String[] args) {
         String str = "cOmpAny NamE is wealthMax finAnCial aDvIce";
         System.out.println(getFirstCapitalLetter(str));
     }
-    public static String getFirstCapitalLetter(String str){
-        String[] str1 = str.split(" ");
-        StringBuilder sb = new StringBuilder();
+    public static Object getFirstCapitalLetter(String str){
+        return Arrays.stream(str.split(" "))
+                .map(e -> Character.toUpperCase(e.charAt(0)) + e.substring(1).toLowerCase())
+                .collect(Collectors.joining(" "));
 
-        for(String s : str1) {
-           sb.append(Character.toUpperCase(s.charAt(0)))
-                   .append(s.substring(1).toLowerCase())
-                   .append(" ");
-        }
-        return sb.toString();
+
+
+//        String[] str1 = str.split(" ");
+//        StringBuilder sb = new StringBuilder();
+//
+//        for(String s : str1) {
+//           sb.append(Character.toUpperCase(s.charAt(0)))
+//                   .append(s.substring(1).toLowerCase())
+//                   .append(" ");
+//        }
+//        return sb.toString();
 
     }
 }
