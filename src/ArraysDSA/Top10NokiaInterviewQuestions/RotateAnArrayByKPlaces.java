e90a3b2f42abf98f02db5bacd239c9ac5e543fe6
majid.f <majid.f@wealthmax.co.uk>
2025-03-03 16:37:05 +0530

    RotateAnArrayByKPlaces

---
+++ src/ArraysDSA/Top10NokiaInterviewQuestions/RotateAnArrayByKPlaces.java
@@ -0,0 +1,28 @@
+package ArraysDSA.Top10NokiaInterviewQuestions;
+
+import java.util.Arrays;
+
+public interface RotateAnArrayByKPlaces {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3 % arr.length;
+        swapAnArray(arr, 0, k -1);
+        swapAnArray(arr, k , arr.length - 1);
+        swapAnArray(arr, 0, arr.length - 1);
+
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    static void swapAnArray(int[] arr, int left, int right) {
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+
+    }
+}
