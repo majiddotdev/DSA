197014b66e3852cbc014392e595be20060132a21
majid.f <majid.f@wealthmax.co.uk>
2025-03-03 14:57:04 +0530

    FindTheDuplicateNumber

--- src/ArraysDSA/Top10NokiaInterviewQuestions/FindTheDuplicateNumber.java
+++ src/ArraysDSA/Top10NokiaInterviewQuestions/FindTheDuplicateNumber.java
@@ -1,6 +1,7 @@
 package ArraysDSA.Top10NokiaInterviewQuestions;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -12,3 +13,14 @@ public class FindTheDuplicateNumber {
         map.entrySet().stream().filter(e->e.getValue() > 1).forEach(e-> System.out.println(e.getKey()));
     }
 }
+
+
+//Map<Integer , Integer> map = new HashMap<>();
+//        for (int i : arr){
+//        if(map.containsKey(i)){
+//        map.put(i , map.get(i) + 1);
+//        }else {
+//        map.put(i , 1);
+//            }
+//                    }
+//                    map.entrySet().stream().filter(e->e.getValue() > 1).forEach(e -> System.out.println(e.getKey()));
