3329239e7bf14f4ecdb9edfeebfddc923648ae0c
majid.f <majid.f@wealthmax.co.uk>
2025-03-03 15:58:50 +0530

    findIntersectionTwoPointers

--- src/ArraysDSA/Top10NokiaInterviewQuestions/FindTheIntersectionOfTwoSortedArrays.java
+++ src/ArraysDSA/Top10NokiaInterviewQuestions/FindTheIntersectionOfTwoSortedArrays.java
@@ -7,19 +7,25 @@ import java.util.List;
 public class FindTheIntersectionOfTwoSortedArrays {
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 4, 5, 6}, arr2 = {2, 3, 5, 7};
-        System.out.println(findTheIntersectionOfTwoSortedArrays(arr1, arr2));
+        System.out.println(findIntersectionTwoPointers(arr1, arr2));
 
     }
 
-    private static List<Integer> findTheIntersectionOfTwoSortedArrays(int[] arr1, int[] arr2) {
+    private static List<Integer> findIntersectionTwoPointers(int[] arr1, int[] arr2) {
         List<Integer> list = new ArrayList<>();
-        for(int i = 0 ; i < arr1.length ; i++){
-            int finalI = i;
-            if( Arrays.stream(arr2).anyMatch(n -> n == arr1[finalI])){
+        int i = 0, j = 0;
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                i++;
+            } else if (arr1[i] > arr2[j]) {
+                j++;
+            } else {
                 list.add(arr1[i]);
-
+                i++;
+                j++;
             }
         }
         return list;
     }
+
 }
