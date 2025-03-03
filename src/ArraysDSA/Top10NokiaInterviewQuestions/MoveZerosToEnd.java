9bf2efddbde32f57ad0f27634230e92d89a022bd
majid.f <majid.f@wealthmax.co.uk>
2025-03-03 15:31:48 +0530

    MoveZerosToEnd using two pointer

--- src/ArraysDSA/Top10NokiaInterviewQuestions/MoveZerosToEnd.java
+++ src/ArraysDSA/Top10NokiaInterviewQuestions/MoveZerosToEnd.java
@@ -4,18 +4,33 @@ import java.util.Arrays;
 
 public class MoveZerosToEnd {
     public static void main(String[] args) {
-        int[] arr = {0, 1, 0, 3, 12};
-        int index = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != 0) {
-                arr[index] = arr[i];
-                index++;
+        int[] arr = {1 ,0, 1, 0, 3, 12};
+        int temp1 = 0, temp2 = 0;
+        while (temp2 != arr.length) {
+            if (arr[temp2] != 0) {
+                int temp = arr[temp1];
+                arr[temp1] = arr[temp2];
+                arr[temp2] = temp;
+                temp1++;
             }
-        }
-        for (int i = index; i < arr.length; i++) {
-            arr[i] = 0;
+            temp2++;
+
+
         }
         System.out.println(Arrays.toString(arr));
+
+
+//        int index = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] != 0) {
+//                arr[index] = arr[i];
+//                index++;
+//            }
+//        }
+//        for (int i = index; i < arr.length; i++) {
+//            arr[i] = 0;
+//        }
+//        System.out.println(Arrays.toString(arr));
 
     }
 }
