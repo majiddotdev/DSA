59610d090f7af0ee54fe852700e4f9d68ea9dde8
majid.f <majid.f@wealthmax.co.uk>
2025-03-03 14:43:39 +0530

    FindTheMissingNumberbyXor

--- src/ArraysDSA/Top10NokiaInterviewQuestions/FindTheMissingNumber.java
+++ src/ArraysDSA/Top10NokiaInterviewQuestions/FindTheMissingNumber.java
@@ -16,4 +16,21 @@ public class FindTheMissingNumber {
         }
         return n * (n + 1) / 2 - sumOfArray;
     }
+
+
+
+
+//    private static int findMissingNumberXOR(int[] arr, int n) {
+//        int xor1 = 0, xor2 = 0;
+//
+//        for (int i = 1; i <= n; i++) {
+//            xor1 ^= i;
+//        }
+//
+//        for (int num : arr) {
+//            xor2 ^= num;
+//        }
+//
+//        return xor1 ^ xor2;
+//    }
 }
