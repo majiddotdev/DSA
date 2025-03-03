b287a22b9a9a49a43f7c99484eb91e30ec750633
majid.f <majid.f@wealthmax.co.uk>
2025-03-03 18:14:32 +0530

    ReverseAString

--- src/ArraysDSA/Top10StringNokiaInterviewQuestions/ReverseAString.java
+++ src/ArraysDSA/Top10StringNokiaInterviewQuestions/ReverseAString.java
@@ -2,9 +2,15 @@ package ArraysDSA.Top10StringNokiaInterviewQuestions;
 
 public interface ReverseAString {
     public static void main(String[] args) {
-        String str = "majidsaba";
-        StringBuilder stringBuffer = new StringBuilder(str);
-        stringBuffer.reverse();
-        System.out.println(stringBuffer);
+        String str = "majidsaba" , str1 = "";
+//        StringBuilder stringBuffer = new StringBuilder(str);
+//        stringBuffer.reverse();
+//        System.out.println(stringBuffer);
+        for(int i = str.length() -1 ; i >= 0 ; i--){
+            str1 += str.charAt(i);
+        }
+
+        System.out.println(str1);
+
     }
 }
