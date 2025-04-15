a486fbe7708eec443a342c994c31f4b6d28ff9b0
majid.f <majid.f@wealthmax.co.uk>
2025-04-15 19:56:04 +0530

    string upper or lower case

--- src/CaptilizeAtParticularIndex.java
+++ src/CaptilizeAtParticularIndex.java
@@ -1,13 +1,29 @@
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class CaptilizeAtParticularIndex {
     public static void main(String[] args) {
-        String str = "hi majid how are you";
-        int index = 2;
-        String result = Arrays.stream(str.split(" "))
-                .map(e->e.substring(0 , 1).concat(String.valueOf(Character.toUpperCase(e.charAt(1)))).concat(e.substring(2)))
-                .collect(Collectors.joining(" "));
-        System.out.println(result);
+        String str = "hi majid how are               you                                          ";
+
+        Arrays.stream(str.trim().toLowerCase().split("\\s+")).map(e->{
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i = 0 ; i< e.length() ; i++){
+                if(i % 2 == 0){
+                    stringBuilder.append(Character.toUpperCase(e.charAt(i)));
+                }else {
+                    stringBuilder.append(Character.toLowerCase(e.charAt(i)));
+
+                }
+            }
+            return stringBuilder.toString();
+
+        }).forEach(System.out::println);
+
+
+
+//        int index = 2;
+//        String result = Arrays.stream(str.split(" "))
+//                .map(e->e.substring(0 , 1).concat(String.valueOf(Character.toUpperCase(e.charAt(1)))).concat(e.substring(2)))
+//                .collect(Collectors.joining(" "));
+//        System.out.println(result);
     }
 }
