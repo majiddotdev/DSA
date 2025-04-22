27cf11b542255860ea4cd33698de166cc05457f8
majid.f <majid.f@wealthmax.co.uk>
2025-04-22 15:47:42 +0530

    Moveallzeroestotheendofthearraywithoutchangingtheorderofnonzeroelements

--- src/dsanewseries/top10arrayquestions/Moveallzeroestotheendofthearraywithoutchangingtheorderofnonzeroelements.java
+++ src/dsanewseries/top10arrayquestions/Moveallzeroestotheendofthearraywithoutchangingtheorderofnonzeroelements.java
@@ -1,4 +1,20 @@
 package dsanewseries.top10arrayquestions;
 
+import java.util.Arrays;
+
 public class Moveallzeroestotheendofthearraywithoutchangingtheorderofnonzeroelements {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 12};
+        int count = 0;
+        for (int i : arr) {
+            if (i != 0)
+                arr[count++] = i;
+
+        }
+        for (int i = count; i < arr.length; i++) {
+            arr[i] = 0;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
 }
