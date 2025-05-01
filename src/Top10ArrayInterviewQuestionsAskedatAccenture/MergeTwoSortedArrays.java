e734d9f334b364aeef628eec20979e33ccfe9912
majid.f <majid.f@wealthmax.co.uk>
2025-05-01 15:18:42 +0530

    MergeTwoSortedArrays

--- src/Top10ArrayInterviewQuestionsAskedatAccenture/MergeTwoSortedArrays.java
+++ src/Top10ArrayInterviewQuestionsAskedatAccenture/MergeTwoSortedArrays.java
@@ -4,17 +4,23 @@ import java.util.Arrays;
 
 public class MergeTwoSortedArrays {
     public static void main(String[] args) {
-        int[] arr1 = {1, 4, 7, 8, 10};
+        int[] arr1 = {1, 4, 7, 8, 10, 11};
         int[] arr2 = {2, 3, 9};
-        int[] arr = new int[arr1.length + arr2.length];
-        int count = arr1.length;
-        for(int i = 0 ; i < arr1.length ; i++){
-            arr[i] = arr1[i];
+        int n = arr1.length, m = arr2.length;
+        int[] arr = new int[n + m];
+        int left = 0, right = 0, count = 0;
+        while (left < n && right < m) {
+            if (arr1[left] < arr2[right]) {
+                arr[count++] = arr1[left++];
+            } else
+                arr[count++] = arr2[right++];
         }
-        for(int i = 0 ; i < arr2.length ; i++){
-            arr[count ++] = arr2[i];
+        while (left < n) {
+            arr[count++] = arr1[left++];
         }
-        Arrays.sort(arr);
+        while (right < m) {
+            arr[count++] = arr2[right++];
+        }
         System.out.println(Arrays.toString(arr));
 
     }
