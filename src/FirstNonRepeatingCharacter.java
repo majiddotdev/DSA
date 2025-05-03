9f43d0904a39cfee848690acbcca13c53ba06c31
majid.f <majid.f@wealthmax.co.uk>
2025-05-03 18:21:12 +0530

    FirstNonRepeatingCharacter

---
+++ src/FirstNonRepeatingCharacter.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstNonRepeatingCharacter {
+    public static void main(String[] args) {
+        String str = "abcdcaf";
+        Map<Character, Integer> countMap = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (countMap.containsKey(str.charAt(i))) {
+                countMap.put(str.charAt(i), countMap.get(str.charAt(i)) + 1);
+            } else {
+                countMap.put(str.charAt(i), 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : countMap.entrySet()) {
+            if (entry.getValue() == 1) {
+                System.out.println("First non-repeating character: " + entry.getKey());
+                return;
+            }
+        }
+    }
+}
