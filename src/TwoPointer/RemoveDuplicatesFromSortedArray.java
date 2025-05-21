a915fb8a70565596ab7336ddfa4b8f8579fa5572
majid.f <majid.f@wealthmax.co.uk>
2025-05-21 19:36:46 +0530

    RemoveDuplicatesFromSortedArray

--- src/TwoPointer/RemoveDuplicatesFromSortedArray.java
+++ src/TwoPointer/RemoveDuplicatesFromSortedArray.java
@@ -1,12 +1,14 @@
 package TwoPointer;
 
+import java.lang.reflect.Array;
+
 public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
         int[] nums = { 0 , 0 , 1 , 1 , 1 , 2 , 2 , 3 , 3 , 4};
         int count = 1 , left = 0 , right = 1 , size = nums.length ;
         while (right < size){
             if(nums[left] != nums[right]){
-                left = right;
+                nums[left] = nums[right];
                 count ++;
             }
             right++;
