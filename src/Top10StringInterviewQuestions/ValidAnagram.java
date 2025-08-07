e70a4aaa0d964c9df2e069fe600596f021fab172
majid.f <majid.f@wealthmax.co.uk>
2025-08-07 18:58:26 +0530

    ValidAnagram

---
+++ src/Top10StringInterviewQuestions/ValidAnagram.java
@@ -0,0 +1,28 @@
+package Top10StringInterviewQuestions;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+        System.out.println(validAnagram("anagram", "nagaram"));
+    }
+
+    private static Boolean validAnagram(String st1, String st2) {
+        if (st1.length() != st2.length()) {
+            return false;
+        }
+
+        int[] arr = new int[26];
+        for (int i = 0; i < st1.length(); i++) {
+            arr[st1.charAt(i) - 'a']++;
+            arr[st2.charAt(i) - 'a']--;
+        }
+        for (int c : arr) {
+            if (c != 0)
+                return false;
+        }
+        return true;
+
+
+    }
+}
